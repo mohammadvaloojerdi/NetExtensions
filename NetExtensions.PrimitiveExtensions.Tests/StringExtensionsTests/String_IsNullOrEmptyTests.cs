f8121bb4253a12fb254d287a07ebc50a66da1a58
Arsalan Valoojerdi <a.valoojerdi@gmail.com>
2020-04-22 21:51:32 +0430

    Add IsNullOrEmpty Extension And Unit Tests

---
+++ NetExtensions.PrimitiveExtensions.Tests/StringExtensionsTests/String_IsNullOrEmptyTests.cs
@@ -0,0 +1,29 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace NetExtensions.PrimitiveExtensions.Tests.StringExtensionsTests
+{
+    public class String_IsNullOrEmptyTests
+    {
+        [Theory]
+        [InlineData("")]
+        [InlineData(null)]
+        public void Should_Be_Able_To_Identify_Null_Or_Empty_Strings(string input)
+        {
+            var result = input.IsNullOrEmpty();
+
+            result.Should().BeTrue();
+        }
+
+        [Theory]
+        [InlineData(" ")]
+        [InlineData("abc")]
+        [InlineData("123")]
+        public void Should_Be_Able_To_Identify_Non_Null_Or_Empty_Strings(string input)
+        {
+            var result = input.IsNullOrEmpty();
+
+            result.Should().BeFalse();
+        }
+    }
+}
--- NetExtensions.PrimitiveExtensions/StringExtensions.cs
+++ NetExtensions.PrimitiveExtensions/StringExtensions.cs
@@ -6,5 +6,10 @@
         {
             return input != null && input.Equals(string.Empty);
         }
+
+        public static bool IsNullOrEmpty(this string input)
+        {
+            return string.IsNullOrEmpty(input);
+        }
     }
 }
