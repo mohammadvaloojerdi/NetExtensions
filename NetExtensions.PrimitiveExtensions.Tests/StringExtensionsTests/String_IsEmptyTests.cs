d361ca96d159ddd72d08dbe1afafa34d6853bb43
Arsalan Valoojerdi <a.valoojerdi@gmail.com>
2020-04-22 21:46:43 +0430

    Add IsEmpty Extension And Unit Tests

---
+++ NetExtensions.PrimitiveExtensions.Tests/StringExtensionsTests/String_IsEmptyTests.cs
@@ -0,0 +1,29 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace NetExtensions.PrimitiveExtensions.Tests.StringExtensionsTests
+{
+    public class String_IsEmptyTests
+    {
+        [Fact]
+        public void Should_Be_Able_To_Identify_Empty_Strings()
+        {
+            var input = "";
+
+            var result = input.IsEmpty();
+
+            result.Should().BeTrue();
+        }
+
+        [Theory]
+        [InlineData(null)]
+        [InlineData(" ")]
+        [InlineData("abc")]
+        public void Should_Be_Able_To_Identify_Non_Empty_Strings(string input)
+        {
+            var result = input.IsEmpty();
+
+            result.Should().BeFalse();
+        }
+    }
+}
---
+++ NetExtensions.PrimitiveExtensions/StringExtensions.cs
@@ -0,0 +1,10 @@
+﻿namespace NetExtensions.PrimitiveExtensions
+{
+    public static class StringExtensions
+    {
+        public static bool IsEmpty(this string input)
+        {
+            return input != null && input.Equals(string.Empty);
+        }
+    }
+}
