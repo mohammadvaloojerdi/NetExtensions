c820693c75f83258ca154b208775b9afcc4f0988
Arsalan Valoojerdi <a.valoojerdi@gmail.com>
2020-04-24 12:10:24 +0430

    Use Dynamic In NumberExtensions

--- NetExtensions.ExtensionMethods/NumberExtensions.cs
+++ NetExtensions.ExtensionMethods/NumberExtensions.cs
@@ -2,12 +2,12 @@
 
 namespace NetExtensions.ExtensionMethods
 {
-    // TODO : Find Better Way Than Convert.ToInt32(number);
+    // TODO : Find Better Way Than Convert To Dynamic
     public static class NumberExtensions
     {
         public static bool IsOdd<T>(this T number) where T : struct, IComparable, IComparable<T>, IConvertible
         {
-            return Convert.ToInt32(number) % 2 != 0;
+            return (dynamic)number % 2 != 0;
         }
 
         public static bool IsEven<T>(this T number) where T : struct, IComparable, IComparable<T>, IConvertible
@@ -17,7 +17,7 @@ namespace NetExtensions.ExtensionMethods
 
         public static bool IsPositive<T>(this T number) where T : struct, IComparable, IComparable<T>, IConvertible
         {
-            return Convert.ToInt32(number) >= 0;
+            return (dynamic)number >= 0;
         }
 
         public static bool IsNegative<T>(this T number) where T : struct, IComparable, IComparable<T>, IConvertible
@@ -27,22 +27,22 @@ namespace NetExtensions.ExtensionMethods
 
         public static bool IsLessThan<T>(this T number, T numberToCompare) where T : struct, IComparable, IComparable<T>, IConvertible
         {
-            return Convert.ToInt32(number) < Convert.ToInt32(numberToCompare);
+            return (dynamic)number < (dynamic)numberToCompare;
         }
 
         public static bool IsLessThanOrEqualTo<T>(this T number, T numberToCompare) where T : struct, IComparable, IComparable<T>, IConvertible
         {
-            return Convert.ToInt32(number) <= Convert.ToInt32(numberToCompare);
+            return (dynamic)number <= (dynamic)numberToCompare;
         }
 
         public static bool IsGreaterThan<T>(this T number, T numberToCompare) where T : struct, IComparable, IComparable<T>, IConvertible
         {
-            return Convert.ToInt32(number) > Convert.ToInt32(numberToCompare);
+            return (dynamic)number > (dynamic)numberToCompare;
         }
 
         public static bool IsGreaterThanOrEqualTo<T>(this T number, T numberToCompare) where T : struct, IComparable, IComparable<T>, IConvertible
         {
-            return Convert.ToInt32(number) >= Convert.ToInt32(numberToCompare);
+            return (dynamic)number >= (dynamic)numberToCompare;
         }
     }
 }
